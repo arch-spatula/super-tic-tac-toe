d0e3f542b0660e2006e054997aafb517d4fcdb4d
arch-spatula <shkim516@naver.com>
2023-11-07 00:00:17 +0900

    test: 승리조건 happy path #15

---
+++ src/util/win.spec.ts
@@ -0,0 +1,71 @@
+import { describe, it, expect, beforeEach, vi } from 'vitest'
+import { checkWin } from './win'
+import { usePlayerTurnStore } from '@/stores/playerTurn'
+import { ref } from 'vue'
+import { createPinia, setActivePinia } from 'pinia'
+
+describe('checkWin', () => {
+  beforeEach(() => {
+    setActivePinia(createPinia())
+  })
+
+  it('가로 승리인지 확인합니다.', () => {
+    const players = usePlayerTurnStore()
+    const blockMarks = ref<MarkType[]>([
+      'empty',
+      'empty',
+      'empty',
+      'O',
+      'O',
+      'O',
+      'empty',
+      'empty',
+      'empty'
+    ])
+    const mock = vi.fn(() => 0)
+
+    checkWin(players.playersTurn, blockMarks, mock)
+
+    expect(mock).toHaveBeenCalledTimes(1)
+  })
+
+  it('세로 승리인지 확인합니다.', () => {
+    const players = usePlayerTurnStore()
+    const blockMarks = ref<MarkType[]>([
+      'O',
+      'empty',
+      'empty',
+      'O',
+      'empty',
+      'empty',
+      'O',
+      'empty',
+      'empty'
+    ])
+    const mock = vi.fn(() => 0)
+
+    checkWin(players.playersTurn, blockMarks, mock)
+
+    expect(mock).toHaveBeenCalledTimes(1)
+  })
+
+  it('대각선 승리인지 확인합니다.', () => {
+    const players = usePlayerTurnStore()
+    const blockMarks = ref<MarkType[]>([
+      'O',
+      'empty',
+      'empty',
+      'empty',
+      'O',
+      'empty',
+      'empty',
+      'empty',
+      'O'
+    ])
+    const mock = vi.fn(() => 0)
+
+    checkWin(players.playersTurn, blockMarks, mock)
+
+    expect(mock).toHaveBeenCalledTimes(1)
+  })
+})
